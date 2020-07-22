28840265224251d299c236f09d73b2a90e9780f8
Cristian Gutierrez <cgutierez@hugeinc.com>
2020-07-21 19:51:03 -0500

    Clase cero agregada

---
+++ clase-repaso/repaso.js
@@ -0,0 +1,35 @@
+var name = "Cristian";
+// console.info(name)
+
+let last_name = "Gutierrez";
+// console.info(last_name)
+
+const age = 25;
+// console.log(age)
+
+// console.log(suma(12, 34));
+
+function suma(numero1, numero2) {
+  return numero1 + numero2;
+}
+
+
+const resta = function (numero1, numero2) {
+  return numero1 - numero2;
+};
+// console.log(resta(34, 12));
+
+const multiplicar = (numero1, numero2) => {
+  return numero1 * numero2;
+};
+
+
+if (5 > 1){
+  let numero = 5
+  let operacion = 5 - 3
+  return operacion
+} else {
+  console.log('Es menor')
+}
+
+5 > 1 ? console.log('Es mayor') : console.log('Es menor')
